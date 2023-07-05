9046b994e86547025e9f9810c3bedca0c0722ea2
Erimsah Olut <erimsaholut@icloud.com>
2023-07-05 15:16:43 +0300

    DataTypes and variables

---
+++ firstCode.jl
@@ -0,0 +1,50 @@
+x = 23 
+y= 31
+ println(x+y)
+ println("My very first codes in julia")
+
+ x=5; y=15; z=515;
+
+Y = x^3 + x*3
+
+x = "Programming in Julia"
+
+z= 5.15
+typeof(x)
+typeof(y)
+typeof(z)
+
+α  = 14.3   # \alpha 
+β = 23      # \beta
+δ = α*β     # \delta
+β₀ = 3      #\beta \_0
+
+println(β₀,"  ",β)
+
+β₀ = 0
+β₁ = 1
+β₂ = 2
+α² = 14.3*14.3
+
+π = 3.31
+ℯ = "euler"
+
+# comment single line
+#= Comment
+             Multi
+                        Line   =#
+
+a,b,c = 5,6,7
+println(a,b,c)
+
+a,b = b,a
+println(a,b)
+
+text::String = "Julia Language"
+
+(7+8)::In
+
+const MYCONST = 42
+
+# MYCONST = 31.5 THATS A ERROR BECAUSE I TRIED TO CHANGE TYPR
+MYCONST = 31
